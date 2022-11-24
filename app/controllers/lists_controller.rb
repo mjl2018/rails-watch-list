793d0c24f0bac18d1b2ec006aaac2324652923c9
Michel Jean Louis <micheljeanlouis.2018@gmail.com>
2022-11-24 16:11:37 -0500

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,20 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @List
+  end
+
+
+  def edit
+  end
 end
